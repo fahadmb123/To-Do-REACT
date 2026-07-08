ea4872c57e45062f38114e9c939aec63305db891
Fahad MB <fahadmbasheer@gmail.com>
2026-07-08 23:46:42 +0530

    The To Do Work Listed Service

--- src/service/toDoService.ts
+++ src/service/toDoService.ts
@@ -1,10 +1,15 @@
 import type { ToDo } from "../types/modal"
-import { save } from "../repository/ToDoRepository"
+import {save,getToDos} from "../repository/ToDoRepository"
 
 
 export function addToDo (toDo:ToDo) {
     save(toDo)
 }
+
+export function getToDo ():ToDo {
+    return getToDos()
+}
+
 
 
 
