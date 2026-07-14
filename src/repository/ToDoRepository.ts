d283ef2b86c7700f01341958323f121f2bf68b0e
Fahad MB <fahadmbasheer@gmail.com>
2026-07-15 01:12:13 +0530

    Updated

--- src/repository/ToDoRepository.ts
+++ src/repository/ToDoRepository.ts
@@ -1,26 +1,17 @@
 import type { ToDo } from "../types/modal";
 
 
-let transaction:number = 0 
 
-
-export function updateTransaction () {
-    transaction++
-}
-
-export function getTransaction () :number {
-    return transaction
-}
 export function save(toDo:ToDo):void{
     const toDos = getToDos()
     toDos.unshift(toDo)
     localStorage.setItem('todos',JSON.stringify(toDos))
-    updateTransaction()
+    
 }
 
 export function saveTodos(todo:ToDo[]) {
     localStorage.setItem("todos",JSON.stringify(todo))
-    updateTransaction()
+    
 }
 
 export function getToDos ():ToDo[] {
